b879768cf2a0871d48643e3b82f96ae08c0b9fff
mmomaster3 <mkbek@mail.ru>
2022-10-21 14:34:01 +0300

    Добавьте файлы проекта.

---
+++ ClaimsTransformer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace fszn_admin
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var wi = (WindowsIdentity)principal.Identity;
+            if (wi.Groups != null)
+            {
+                foreach (var group in wi.Groups) //-- Getting all the AD groups that user belongs to---  
+                {
+                    try
+                    {
+                        var claim = new Claim(wi.RoleClaimType, group.Value);
+                        wi.AddClaim(claim);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            return Task.FromResult(principal);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace fszn_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
